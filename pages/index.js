38d37d8e95b9ca7eeced7f18b58f4939bf738b60
Daniel Silver <dabzbm@gmail.com>
2021-04-10 13:05:08 -0400

    'head data fixed'

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Next JS Template</title>
+        <title>Crypto Track || by SilverStack</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Hero>
